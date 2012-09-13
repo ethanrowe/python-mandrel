f7ab4c278c0b2a2d90fb88055329369870964295
Ethan Rowe <erowe@cargometrics.com>
2012-09-13 15:56:38 -0400

    Setup tweaks for initial release
    Tweak build tag to -2, working through some initial build issues.
    
    Removed data_file dependencies as they're giving problems with
    the source distribution; move part of README and all of LICENSE into
    the long description.

--- setup.py
+++ setup.py
@@ -1,10 +1,6 @@
 from setuptools import setup
 import os
 
-def readme():
-    path = os.path.join(os.path.dirname(__file__), 'README.md')
-    return open(path, 'r').read()
-
 setup(
     name = "mandrel",
     version = "0.0.1",
@@ -15,7 +11,32 @@ setup(
     keywords = "bootstrap configuration setup",
     url = "https://github.com/ethanrowe/python-mandrel",
     packages=['mandrel'],
-    long_description=readme(),
+    long_description="""
+# Mandrel (python-mandrel) #
+
+Mandrel provides bootstrapping and configuration tools for consistent,
+straightforward project config management.
+
+Use Mandrel to:
+
+* bootstrap your python project configuration
+* manage configuration file location/access
+* manage logging configuration and Logger retrieval
+
+Separate projects can rely on Mandrel for these purposes, and when they're
+brought together (as eggs, for instance) to a single application, their
+configuration can be managed simply in an easily-configurable way.
+
+# License #
+
+Copyright (c) 2012 Ethan Rowe <ethan at the-rowes dot com>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+""",
     test_suite='mandrel.test',
     classifiers=[
         "Development Status :: 3 - Alpha",
@@ -30,9 +51,6 @@ setup(
     install_requires=[
         'mock',
         'PyYAML',
-    ],
-    data_files=[
-        'LICENSE.txt',
     ],
 )
 
