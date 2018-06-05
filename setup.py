911e3843e5c141e1c9a5477b4c817d7b96d01654
Ethan Rowe <ethanrowe000@gmail.com>
2018-06-05 12:29:59 -0400

    Bump to version 1.0.0!
    Just in time to tell people to please please please not use this
    project.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 
 setup(
     name = "mandrel",
-    version = "0.3.0",
+    version = "1.0.0",
     author = "Ethan Rowe",
     author_email = "ethan@the-rowes.com",
     description = ("Provides bootstrapping for sane configuration management"),
